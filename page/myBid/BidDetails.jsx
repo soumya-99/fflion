ee7fc956fe5685de540e5e477580c024d6f39389
soumya-99 <soumyadeepmondal1234@gmail.com>
2024-01-18 15:50:04 +0530

    BidDetails screen redesigned complete

--- page/myBid/BidDetails.jsx
+++ page/myBid/BidDetails.jsx
@@ -5,6 +5,7 @@ import {
   ScrollView,
   TouchableOpacity,
   PixelRatio,
+  SafeAreaView,
 } from 'react-native';
 import React, {useState, useContext} from 'react';
 import {authstyles} from '../style/pagestyle';
@@ -15,6 +16,8 @@ import DatePicker from 'react-native-date-picker';
 import axios from 'axios';
 import {BASE_URL} from '../../src/config';
 import {AuthContext} from '../../src/context/AuthContext';
+import LinearGradient from 'react-native-linear-gradient';
+import normalize, {SCREEN_HEIGHT, SCREEN_WIDTH} from 'react-native-normalize';
 
 const BidDetails = ({navigation, route}) => {
   const {itemData, game_name} = route.params;
@@ -45,81 +48,84 @@ const BidDetails = ({navigation, route}) => {
       .catch(error => console.error(error));
   };
   return (
-    <View style={authstyles.container}>
-      <View style={authstyles.title}>
+    <SafeAreaView style={{marginBottom: SCREEN_HEIGHT / 18}}>
+      <View>
         <TitleBar />
       </View>
-      <View style={authstyles.body}>
-        <View style={styles.list_container}>
-          {/* <View style={styles.comp1}>
-           <OfferText/>
-          </View> */}
-          <View style={styles.comp2}>
-            <Banner />
-          </View>
-          {/* <View style={styles.comp3}>
-            <TransComp />
-          </View> */}
-        </View>
-        <View style={styles.list_container2}>
-          {/* Game Name */}
-
-          <View style={{marginBottom: 20}}>
+      <ScrollView>
+        <LinearGradient
+          start={{x: 0, y: 1}}
+          end={{x: 1, y: 0}}
+          colors={['#5ce1ff', '#8c1e96', '#1b2196']}
+          style={styles.linearGradientBg}>
+          <View style={{marginBottom: normalize(20), padding: normalize(20)}}>
             <Text
               style={{
                 textAlign: 'center',
-                color: 'red',
-                fontSize: 30,
+                color: '#FFFFFF',
+                fontSize: normalize(30),
                 fontWeight: '600',
               }}>
               {game_name}
             </Text>
-          </View>
-
-          {/* ROW of date and show button */}
-          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <TouchableOpacity
-              onPress={() => setOpen(true)}
-              style={{backgroundColor: '#d2bdc8'}}>
-              <Text
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-around',
+                marginTop: SCREEN_HEIGHT / 20,
+              }}>
+              <TouchableOpacity
+                onPress={() => setOpen(true)}
                 style={{
-                  color: 'black',
-                  fontWeight: '600',
-                  fontSize: 16,
-                  paddingHorizontal: 10,
-                  paddingVertical: 5,
+                  width: SCREEN_WIDTH / 3,
+                  height: SCREEN_HEIGHT / 20,
+                  backgroundColor: 'lavender',
+                  borderRadius: 15,
+                  justifyContent: 'center',
+                  alignItems: 'center',
                 }}>
-                Date : - {date.toLocaleDateString()}
-              </Text>
-              <DatePicker
-                modal
-                mode="date"
-                open={open}
-                date={date}
-                onConfirm={date => {
-                  setOpen(false);
-                  setDate(date);
-                }}
-                onCancel={() => {
-                  setOpen(false);
-                }}
-              />
-            </TouchableOpacity>
-
-            <TouchableOpacity
-              onPress={handleGetBidList}
-              style={{backgroundColor: '#d2bdc8'}}>
-              <Text
+                <Text
+                  style={{
+                    color: '#000000',
+                    textTransform: 'uppercase',
+                    fontWeight: '700',
+                  }}>
+                  {date.toLocaleDateString('en-GB')}
+                </Text>
+                <DatePicker
+                  modal
+                  mode="date"
+                  open={open}
+                  date={date}
+                  onConfirm={date => {
+                    setOpen(false);
+                    setDate(date);
+                  }}
+                  onCancel={() => {
+                    setOpen(false);
+                  }}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={handleGetBidList}
                 style={{
-                  color: 'black',
-                  fontWeight: '600',
-                  fontSize: 16,
-                  paddingHorizontal: 10,
-                  paddingVertical: 5,
+                  width: SCREEN_WIDTH / 3,
+                  height: SCREEN_HEIGHT / 20,
+                  backgroundColor: 'gold',
+                  borderRadius: 15,
+                  justifyContent: 'center',
+                  alignItems: 'center',
                 }}>
-                Show
-              </Text>
-            </TouchableOpacity>
+                <Text
+                  style={{
+                    color: '#000000',
+                    textTransform: 'uppercase',
+                    fontWeight: '700',
+                  }}>
+                  Show
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
 
           {/* Table */}
@@ -131,14 +137,14 @@ const BidDetails = ({navigation, route}) => {
               borderWidth: 1,
               borderTopRightRadius: 5,
               borderTopLeftRadius: 5,
-              backgroundColor: '#d2bdc8',
-              marginTop: 30,
+              backgroundColor: '#6ce1ffbb',
+              // marginTop: 30,
             }}>
             {/* blanck text */}
             <Text
               style={{
                 ...styles.cellText,
-                width: 78,
+                width: 98,
                 textAlign: 'center',
               }}>
               {' Date '}
@@ -178,161 +184,124 @@ const BidDetails = ({navigation, route}) => {
               {'Status'}
             </Text>
           </View>
-          <ScrollView>
-            {gameBidArray &&
-              gameBidArray.map((props, index) => {
-                let newdate = props.created_at
-                  ? new Date(props.created_at)
-                  : false;
 
-                return (
+          {gameBidArray &&
+            gameBidArray.map((props, index) => {
+              let newdate = props.created_at
+                ? new Date(props.created_at)
+                : false;
+
+              // console.log(
+              //   '19593465316589761387563485837468957362',
+              //   props.created_at,
+              // );
+              // console.log('19593465316589761387563485837468957362', props);
+
+              return (
+                <View
+                  key={index}
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'space-around',
+                    borderWidth: 1,
+                    backgroundColor: '#FFFFFFAA',
+                  }}>
                   <View
-                    key={index}
                     style={{
-                      flexDirection: 'row',
-                      alignItems: 'center',
-                      justifyContent: 'space-around',
-                      borderWidth: 1,
-                      backgroundColor: '#fff',
+                      // paddingHorizontal: 2,
+                      height: 30,
+                      justifyContent: 'center',
+                      borderRadius: 2,
                     }}>
+                    <Text
+                      style={{
+                        fontWeight: '700',
+                        fontSize: 12,
+                        color: '#FFFFFF',
+                        width: 55,
+                      }}>
+                      {newdate && newdate?.toLocaleDateString()}
+                    </Text>
+                  </View>
+                  {/* table start */}
+                  <View>
                     <View
                       style={{
-                        paddingHorizontal: 2,
-                        height: 30,
-                        justifyContent: 'center',
-                        borderRadius: 2,
+                        flexDirection: 'row',
+                        justifyContent: 'space-around',
+                        alignItems: 'center',
+                        borderBottomWidth: 1,
+                        borderStyle: 'dashed',
                       }}>
-                      <Text
-                        style={{
-                          fontWeight: '700',
-                          fontSize: 12,
-                          color: 'black',
-                          width: 50,
-                        }}>
-                        {newdate && newdate?.toLocaleDateString()}
+                      {/* single Text */}
+                      <Text style={styles.cellText}>
+                        {props.single || ' - '}
+                      </Text>
+                      {/* Juri Text */}
+                      <Text style={styles.cellText}>{props.jodi || ' - '}</Text>
+                      {/* Patti Text */}
+                      <Text style={{...styles.cellText, borderRightWidth: 1}}>
+                        {props.patti || ' - '}
                       </Text>
                     </View>
-                    {/* table start */}
-                    <View>
-                      <View
-                        style={{
-                          flexDirection: 'row',
-                          justifyContent: 'space-around',
-                          alignItems: 'center',
-                          borderBottomWidth: 1,
-                          borderStyle: 'dashed',
-                        }}>
-                        {/* single Text */}
-                        <Text style={styles.cellText}>
-                          {props.single || ' - '}
-                        </Text>
-                        {/* Juri Text */}
-                        <Text style={styles.cellText}>
-                          {props.jodi || ' - '}
-                        </Text>
-                        {/* Patti Text */}
-                        <Text style={{...styles.cellText, borderRightWidth: 1}}>
-                          {props.patti || ' - '}
-                        </Text>
-                      </View>
-                      <View
-                        style={{
-                          flexDirection: 'row',
-                          justifyContent: 'space-around',
-                          alignItems: 'center',
-                        }}>
-                        {/* single Text */}
-                        <Text style={styles.cellText}>{props.single_amt}</Text>
-                        {/* Juri Text */}
-                        <Text style={styles.cellText}>{props.jodi_amt}</Text>
-                        {/* Patti Text */}
-                        <Text style={{...styles.cellText, borderRightWidth: 1}}>
-                          {props.patti_amt}
-                        </Text>
-                      </View>
-                    </View>
-
-                    {/* delete button */}
                     <View
                       style={{
-                        paddingHorizontal: 2,
-                        height: 30,
-                        justifyContent: 'center',
-                        borderRadius: 2,
+                        flexDirection: 'row',
+                        justifyContent: 'space-around',
+                        alignItems: 'center',
                       }}>
-                      <Text
-                        style={{
-                          fontWeight: '700',
-                          fontSize: 12,
-                          color: 'black',
-                        }}>
-                        {props.trns_flag == 'WI' ? 'WIN' : '      '}
+                      {/* single Text */}
+                      <Text style={styles.cellText}>{props.single_amt}</Text>
+                      {/* Juri Text */}
+                      <Text style={styles.cellText}>{props.jodi_amt}</Text>
+                      {/* Patti Text */}
+                      <Text style={{...styles.cellText, borderRightWidth: 1}}>
+                        {props.patti_amt}
                       </Text>
                     </View>
                   </View>
-                );
-              })}
-          </ScrollView>
-        </View>
-      </View>
-    </View>
+
+                  {/* delete button */}
+                  <View
+                    style={{
+                      paddingHorizontal: 2,
+                      height: 30,
+                      justifyContent: 'center',
+                      borderRadius: 2,
+                    }}>
+                    <Text
+                      style={{
+                        fontWeight: '700',
+                        fontSize: 12,
+                        color: 'black',
+                      }}>
+                      {props.trns_flag == 'WI' ? 'WIN' : '      '}
+                    </Text>
+                  </View>
+                </View>
+              );
+            })}
+        </LinearGradient>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default BidDetails;
 
 const styles = StyleSheet.create({
-  list_container: {
-    // flex: 1
+  linearGradientBg: {
+    height: SCREEN_HEIGHT,
+    alignItems: 'center',
+    // padding: normalize(10),
   },
-  list_container2: {
-    // flex: 4
-  },
-  comp1: {
-    // flex: 1
-  },
-  comp2: {
-    // flex: 5
-  },
-  comp3: {
-    // flex: 3
-  },
-  inputContainer: {
-    backgroundColor: '#3fc367',
-    width: '100%',
-    borderRadius: 10,
-    elevation: 8,
-    borderWidth: 0.5,
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    paddingVertical: 10,
-  },
-  titleStyle: {
-    textAlign: 'center',
-    fontWeight: '900',
-    color: 'black',
-  },
-  textInputStyle: {
-    width: PixelRatio.roundToNearestPixel(95),
-    maxWidth: PixelRatio.roundToNearestPixel(95),
-    height: 40,
-    margin: 5,
-    marginVertical: 4,
-    borderWidth: 1,
-    padding: 10,
-    borderRadius: 10,
-    borderWidth: 0,
-    backgroundColor: 'white',
-    color: 'black',
-    elevation: 5,
-  },
-
   cellText: {
     borderLeftWidth: 1,
     paddingHorizontal: 2,
     width: 65,
     fontWeight: '500',
-    color: 'black',
+    color: '#FFFFFF',
     textAlign: 'center',
   },
 });
