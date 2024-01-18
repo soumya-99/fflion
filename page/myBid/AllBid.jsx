50c0c73252eaf24482408724daaf58bc5abeb668
soumya-99 <soumyadeepmondal1234@gmail.com>
2024-01-18 13:19:37 +0530

    AllBid Screen redesigned done

--- page/myBid/AllBid.jsx
+++ page/myBid/AllBid.jsx
@@ -1,4 +1,12 @@
-import {StyleSheet, Text, View, TouchableOpacity, FlatList} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+} from 'react-native';
 import React, {useContext, useEffect, useState} from 'react';
 import {authstyles} from '../style/pagestyle';
 import TitleBar from '../../component/titlebar/TitleBar';
@@ -8,6 +16,8 @@ import OfferText from '../../component/offerText/OfferText';
 import Banner from '../../component/banner/Banner';
 import TransComp from '../../component/trans_component/TransComp';
 import {useIsFocused} from '@react-navigation/native';
+import LinearGradient from 'react-native-linear-gradient';
+import normalize, {SCREEN_HEIGHT, SCREEN_WIDTH} from 'react-native-normalize';
 
 const AllBid = ({navigation, route}) => {
   const {userInfo} = useContext(AuthContext);
@@ -25,7 +35,7 @@ const AllBid = ({navigation, route}) => {
       .catch(error => console.error(error));
   }, [isFocused]);
 
-  const GameTimeList = ({item, index}) => {
+  const GameListTwo = ({item, index}) => {
     // console.log("hello  ", item)
     const ordinalNumber =
       index + 1 === 1
@@ -35,129 +45,116 @@ const AllBid = ({navigation, route}) => {
         : index + 1 === 3
         ? '3rd'
         : `${index + 1}th`;
-    return (
-      <>
-        {/* Main Cointainer */}
 
-        <TouchableOpacity
-          onPress={() =>
-            navigation.navigate('BidDetails', {
-              itemData: item,
-              game_name,
-            })
-          }
+    return (
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: normalize(8),
+        }}>
+        <LinearGradient
+          start={{x: 1, y: 0}}
+          end={{x: 0, y: 2}}
+          colors={['#8c1e96', '#1b2196']}
           style={{
-            backgroundColor: 'rgba(255, 255, 255, 0.2)',
             flexDirection: 'row',
             justifyContent: 'space-between',
-            paddingVertical: 10,
-            paddingHorizontal: 15,
             alignItems: 'center',
-            marginBottom: 10,
-            marginTop: 10,
-            marginLeft: 10,
-            borderColor: 'rgba(255, 255, 255, 0.5)',
-            borderWidth: 1,
+            width: SCREEN_WIDTH / 1.04,
+            height: SCREEN_HEIGHT / 10,
+            borderRadius: normalize(10),
           }}>
-          {/* Game Name */}
-          <View style={{flexDirection: 'column'}}>
+          <View
+            style={{
+              width: '40%',
+              height: '70%',
+              paddingLeft: normalize(20),
+              flexDirection: 'column',
+              justifyContent: 'center',
+            }}>
             <Text
               numberOfLines={1}
               ellipsizeMode="tail"
-              style={{fontSize: 16, fontWeight: 'bold', color: 'white'}}>
+              style={{fontSize: 13, fontWeight: 'bold', color: '#FFFFFF'}}>
               GAME TIME
             </Text>
             <Text
               numberOfLines={1}
               ellipsizeMode="tail"
-              style={{fontSize: 16, fontWeight: 'bold', color: 'white'}}>
+              style={{fontSize: 13, fontWeight: 'bold', color: '#FFFFFF'}}>
               {item.game_time}
             </Text>
             <Text
               numberOfLines={2}
               ellipsizeMode="tail"
-              style={{fontSize: 12, fontWeight: 'bold', color: 'white'}}>
+              style={{fontSize: 12, fontWeight: 'bold', color: '#FFFFFF'}}>
               RESULT : {item.result_time}
             </Text>
           </View>
-
-          <View>
-            <Text
+          <View style={{width: '20%'}}>
+            <Text style={{color: '#FFFFFF', fontWeight: '700'}}>
+              {ordinalNumber} Bazi
+            </Text>
+          </View>
+          <View style={{paddingRight: normalize(35)}}>
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate('BidDetails', {
+                  itemData: item,
+                  game_name,
+                })
+              }
               style={{
-                fontSize: 16,
-                fontWeight: 'bold',
-                color: 'white',
-                letterSpacing: 1,
+                height: '50%',
+                width: '140%',
+                backgroundColor: '#0b9c92',
+                borderRadius: normalize(10),
+                justifyContent: 'center',
+                alignItems: 'center',
               }}>
-              {ordinalNumber} Baji
-            </Text>
+              <Text style={{fontWeight: '600', color: '#FFFFFF'}}>SHOW</Text>
+            </TouchableOpacity>
           </View>
-
-          {/* Play container */}
-          <View
-            style={{
-              backgroundColor: 'yellow',
-              paddingVertical: 5,
-              paddingHorizontal: 10,
-              borderRadius: 5,
-            }}>
-            <Text style={{fontSize: 16, fontWeight: 'bold', color: 'black'}}>
-              Show
-            </Text>
-          </View>
-        </TouchableOpacity>
-      </>
+        </LinearGradient>
+      </View>
     );
   };
 
   return (
-    <View style={authstyles.container}>
-      <View style={authstyles.title}>
+    <SafeAreaView style={{marginBottom: SCREEN_HEIGHT / 18}}>
+      <View>
         <TitleBar />
       </View>
-      <View style={authstyles.body}>
-        {/* <Text>Profile</Text> */}
-        <View style={styles.list_container}>
-          {/* <View style={styles.comp1}>
-                        <OfferText />
-                    </View> */}
-          <View style={styles.comp2}>
-            <Banner />
-          </View>
-          {/* <View style={styles.comp3}>
-                        <TransComp />
-                    </View> */}
-        </View>
-        <View style={styles.list_container2}>
-          <FlatList
+      <ScrollView>
+        <LinearGradient
+          start={{x: 0, y: 1}}
+          end={{x: 1, y: 0}}
+          colors={['#5ce1ff', '#8c1e96', '#1b2196']}
+          style={styles.linearGradientBg}>
+          {/* <FlatList
             data={gameTime}
             keyExtractor={item => item.game_id}
             renderItem={({item, index}) => (
-              <GameTimeList item={item} index={index} />
+              <GameListTwo item={item} index={index} />
             )}
-          />
-        </View>
-      </View>
-    </View>
+          /> */}
+
+          {gameTime?.map((item, index) => (
+            <GameListTwo key={item.game_id} item={item} index={index} />
+          ))}
+        </LinearGradient>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default AllBid;
 
 const styles = StyleSheet.create({
-  list_container: {
-    // flex: 1
-  },
-  list_container2: {
-    // flex: 4
-  },
-  // comp1:{
-  //     flex:1
-  //   },
-  comp2: {
-    // flex:6
-  },
-  comp3: {
-    // flex:3
+  linearGradientBg: {
+    height: SCREEN_HEIGHT,
+    alignItems: 'center',
+    // padding: normalize(10),
   },
 });
