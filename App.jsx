bdbb1de0866e7014e592efa0c483fe7b7e13618c
soumya-99 <soumyadeepmondal1234@gmail.com>
2024-01-29 11:41:23 +0530

    Push Notification title added

--- App.jsx
+++ App.jsx
@@ -1,36 +1,35 @@
-import { StyleSheet, Text, View, ScrollView, SafeAreaView, StatusBar, Alert } from 'react-native'
-import { NavigationContainer } from "@react-navigation/native";
-import React, { useEffect } from 'react'
-import { StackNavigator } from "./src/navigation/StackNavigator";
-import BottomTabNavigator from "./src/navigation/TabNavigator";
-import DrawerNavigator from "./src/navigation/DrawerNavigator";
-import { AuthProvider } from './src/context/AuthContext';
+import {Alert} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import {StackNavigator} from './src/navigation/StackNavigator';
+import {AuthProvider} from './src/context/AuthContext';
 import messaging from '@react-native-firebase/messaging';
-import PushNotification from 'react-native-push-notification';
 
-import {check,request, PERMISSIONS, RESULTS} from 'react-native-permissions';
-
-import { PermissionsAndroid } from 'react-native' 
-
-import {getToken, notificationListenr, requestUserPermission} from './src/core/commonUtils';
+import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
+import {
+  getToken,
+  notificationListenr,
+  requestUserPermission,
+} from './src/core/commonUtils';
 
 const App = () => {
   useEffect(() => {
     const unsubscribe = messaging().onMessage(async remoteMessage => {
-      console.log(remoteMessage)
-     
-      Alert.alert('A new FCM message arrived!', `Title : ${remoteMessage.notification.body} `);
+      console.log(remoteMessage);
+
+      Alert.alert(
+        `${remoteMessage.notification.title}`,
+        `Title : ${remoteMessage.notification.body} `,
+      );
     });
     return unsubscribe;
   }, []);
-
- 
 
   const requestNotificationPermission = async () => {
     const result = await request(PERMISSIONS.ANDROID.POST_NOTIFICATIONS);
     return result;
   };
-  
+
   const checkNotificationPermission = async () => {
     const result = await check(PERMISSIONS.ANDROID.POST_NOTIFICATIONS);
     return result;
@@ -39,11 +38,11 @@ const App = () => {
   const requestPermission = async () => {
     const checkPermission = await checkNotificationPermission();
     if (checkPermission !== RESULTS.GRANTED) {
-     const request = await requestNotificationPermission();
-       if(request !== RESULTS.GRANTED){
-            // permission not granted
-            console.log("error")
-        }
+      const request = await requestNotificationPermission();
+      if (request !== RESULTS.GRANTED) {
+        // permission not granted
+        console.log('error');
+      }
     }
   };
 
@@ -52,7 +51,6 @@ const App = () => {
     requestUserPermission();
     notificationListenr();
     getToken();
-
   }, []);
   return (
     <AuthProvider>
@@ -60,9 +58,7 @@ const App = () => {
         <StackNavigator />
       </NavigationContainer>
     </AuthProvider>
-  )
-}
+  );
+};
 
-export default App
-
-const styles = StyleSheet.create({})
+export default App;
